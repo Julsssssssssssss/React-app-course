1474118105b8e35211994fb33d00b623a0af964f
Julsssssssssssss <kiev2006julia@yandex.ru>
2026-04-22 17:19:02 +0300

    dev: home page, questionCard

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+// import cls from "./HomePage.module.css";
+import {} from "react";
+// import cls from "./HomePage.module.css";
+import { QuestionCard } from "../../components/QuestionCard/QuestionCard";
+
+export const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <QuestionCard />
+    </div>
+  );
+};
