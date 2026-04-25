16b0f81226c681ff6527c18ba8a45a64b4643daa
Julsssssssssssss <kiev2006julia@yandex.ru>
2026-04-25 15:09:46 +0300

    delayFn, useFetch

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,28 +4,27 @@ import { useState, useEffect } from "react";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
+// import { delayFn } from "../../helpers/delayFn";
+import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions, error] = useState([]);
+  const [getQuestions, isLoading] = useFetch(async (url) => {
+    const response = await fetch(`${API_URL}/${url}`);
+    const questions = await response.json();
 
-  const getQuestions = async () => {
-    try {
-      const response = await fetch(`${API_URL}/react`);
-      const questions = await response.json();
-      setQuestions(questions);
-      console.log("questions", questions);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    setQuestions(questions);
+    return questions;
+  });
 
   useEffect(() => {
-    getQuestions();
+    getQuestions("react");
   }, []);
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
     </>
   );
