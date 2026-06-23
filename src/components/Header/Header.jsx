9444ec83fcbf9721f1634da74217faea65a93cf6
Julsssssssssssss <kiev2006julia@yandex.ru>
2026-06-23 20:06:17 +0300

    theme context, theme toggler, darkLayout class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import { Button } from "../Button";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAyth";
 import { AUTH_STORAGE } from "../../constants";
+import { ThemeToggler } from "../../features/ThemeToggler";
 
 export const Header = () => {
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ export const Header = () => {
         <span>ReactCards</span>
       </p>
       <div className={cls.headerButtons}>
+        <ThemeToggler />
         {isAuth && <Button onClick={() => navigate("/addquestion")}>Add</Button>}
         <Button onClick={loginHandler} isActive={!isAuth}>
           {isAuth ? "Logout" : "Login"}
---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+import { THEME_STORAGE } from "../constants";
+
+export const ThemeContext = createContext(null);
+
+export const ThemeProvider = ({ children }) => {
+  const savedTheme = localStorage.getItem(THEME_STORAGE) || true;
+  const [theme, setTheme] = useState(savedTheme);
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+};
